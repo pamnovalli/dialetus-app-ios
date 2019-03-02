d1db702cd77920c327c10724269e33644df98d0a
Pamela Ianovalli <pamela.novalli@gmail.com>
2019-03-02 14:24:10 -0300

    Project configuration

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'dialetus-ios' do
+   
+end
---
+++ dialetus-iosTests/dialetus_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  dialetus_iosTests.swift
+//  dialetus-iosTests
+//
+//  Created by Felipe Antonio Cardoso on 02/03/19.
+//  Copyright © 2019 Pamela Ianovalli. All rights reserved.
+//
+
+import XCTest
+@testable import dialetus_ios
+
+class dialetus_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
